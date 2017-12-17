211082bbde3a35157434412714cf28209f36b09a
OscarLawton <emmetlawton@gmail.com>
2017-12-17 14:58:56 +0000

    Work in progress

--- game.rb
+++ game.rb
@@ -10,48 +10,61 @@ class Game
   File.open("5desk.txt").each { |line| @@dic.push(line) if line.length > 5 && line.length <14}
   @word = " "
   @gameWon = false
-  def initialize 
-    puts "This ran!"
-    @word = @@dic[rand(0..48891)] 
-    gameLoop
+  def initialize
+    
+    $random_word_array = @@dic[rand(0..48891)].downcase.split('')
+    $random_word_array.delete_at($random_word_array.length-1)
+   
   end
   
-  def gameLoop
-    while !@gameWon
-      getGuess
-      displayRandomWord
-    end
-  end
+ 
   
   def getGuess
+    
     oneLetter = false
-    while !oneLetter
+    alreadyGuessed = false
+    goodChars = false
+    
+    while !oneLetter && !alreadyGuessed && !goodChars
+      print "Your guesses are :"
+      print $guess_array
       puts "Enter a single letter as your choice: "
       guess = gets.chomp
+      guess.downcase
       puts guess.length
+      
+      $guess_array.include?(guess) ? alreadyGuessed = true : $guess_array.push(guess)
+      puts "You already guessed that letter, guess again." if alreadyGuessed
+      
       oneLetter = true if guess.length == 1
+      puts "One charater at a time, guess again." if oneLetter
+      
+      (guess.ord > 64 && guess.ord < 91 ) || (guess.ord > 96 && guess.ord < 123) || guess.ord == 49 ? goodChars = true : goodChars = false
+      puts "Only letter please, guess again." if goodChars == false
+   
     end
-    
-    $guess_array.push(guess)
+      true if guess == "1"
   end
   
   def checkGuess
     $random_word_array.each do |letter|
-      
+       $guess_array.include?(letter) ? true : false
     end
   end
   
   def displayRandomWord
     
-    $random_word_array = @word.split('')
-    $random_word_array.delete_at($random_word_array.length-1)
+    
+    
     print $random_word_array
     puts
-    print $guess_array
-    puts $random_word_array =~ /$guess_array[0]/
+    
+  
     $random_word_array.each do |letter|
-      puts letter
+      print $guess_array.include?(letter) ? letter : "_"
     end
+   
+    puts
   end
   
   def getWord
--- main.rb
+++ main.rb
@@ -4,19 +4,23 @@ puts "Welcome to Hangman"
 
 
 quit = false
+count = 12
 while quit == false
   
   puts "Enter 1 for new game, 2 to load a game and 3 for quit: "
   choice = gets.chomp
- 
+  
   case choice
   when "1"
-   
-    
-    
     game = Game.new
-   
-  
+    while !quit
+      puts "You have #{count} guesses left!"
+      quit = game.getGuess
+      game.displayRandomWord
+      count -= 1
+      
+    end
+    quit = true if count == 0
   when "2"
     #Open a saved file here
   when "3"
