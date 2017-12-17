d76ce056ca7532d4d2448758d43e7f115dce6cba
OscarLawton <emmetlawton@gmail.com>
2017-12-17 12:34:43 +0000

    Work still in progress

--- game.rb
+++ game.rb
@@ -2,14 +2,31 @@ require 'json'
 
 class Game
  
-  @word = " "
-  def initialize randomWord
-    
-    @word = randomWord
   
+  @@dic = []
+  @guess_array = []
+  File.open("5desk.txt").each { |line| @@dic.push(line) if line.length > 5 && line.length <14}
+  @word = " "
+  
+  def initialize 
+    puts "This ran!"
+    @word = @@dic[rand(0..48891)] 
+    
   end
   
-  def getName
+  def getGuess
+    oneLetter = false
+    while !oneLetter
+      puts "Enter a single letter as your choice: "
+      guess = gets.chomp
+      puts guess.length
+      oneLetter = true if guess.length == 1
+    end
+    
+    @guess_array.push(guess)
+  end
+  
+  def getWord
     @word
   end
   
--- main.rb
+++ main.rb
@@ -2,28 +2,25 @@ require_relative "game.rb"
 
 puts "Welcome to Hangman"
 
-dic = []
-File.open("5desk.txt").each { |line| dic.push(line) if line.length > 5 && line.length <14}
+
 quit = false
-newGame = false
 while quit == false
   
-  puts "Enter 1 for new game, 2 for load a saved game, 3 to load a game and 4 for quit: "
+  puts "Enter 1 for new game, 2 to load a game and 3 for quit: "
   choice = gets.chomp
  
   case choice
   when "1"
    
-    randWord = dic[rand(0..48891)]
     
-    game = Game.new(randWord)
     
-    newGame = true
+    game = Game.new
+    game.getGuess
+    
+  
   when "2"
     #Open a saved file here
-  when "2"
-    
-  when "4"
+  when "3"
     quit = true
   end
   
